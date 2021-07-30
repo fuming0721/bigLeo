13ea7dd5d8bd2cffb181fb2d9804d92577f8cad2
付铭 <fm3200@163.com>
2021-07-30 16:58:02 +0800

    m

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="login-container">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+*
+  padding: 0;
+  margin: 0;
+
+.login-container
+  display flex
+  align-items center
+  justify-content center
+  position fixed
+  width 100vw
+  height 100vh
+  margin 0 auto
+  background url("http://www.17sucai.com/preview/242158/2015-01-10/%E7%99%BB%E5%BD%95/images/cloud.jpg") 0 bottom repeat-x #049ec4
+  animation animate-cloud 10s linear infinite
+
+@keyframes animate-cloud
+  from
+    background-position 0 100%
+  to
+    background-position 600px 100%
 </style>
